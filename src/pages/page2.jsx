006b3997155b0d839d3a1e8005ba088fdb4503e0
Mladen1996 <mladjalug96@gmail.com>
2020-01-12 14:22:44 +0100

    Update page2.jsx

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -6,14 +6,14 @@ import { getLocaleCopy } from '~src/utils/helpers';
 import { page2 } from '~data/pages';
 
 const SecondPage = ({ pageContext: { locale, name } }) => {
-  const { siteTitle, h1Title, text, linkLabel, linkSlug } = getLocaleCopy(
+  const {h1Title, text, linkLabel, linkSlug } = getLocaleCopy(
     page2,
     locale
   );
 
   return (
-    <Layout name={name} title={siteTitle}>
-      <SEO title={siteTitle} />
+    <Layout name={name}>
+     
       <h1>{h1Title}</h1>
       <p>{text}</p>
       <Link to={linkSlug}>{linkLabel}</Link>
