698dadd1d0e47388440706114827c8c981316353
Mladen1996 <mladjalug96@gmail.com>
2020-01-12 14:36:26 +0100

    Delete page2.jsx

--- src/pages/page2.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from '~src/components/Layout/PageLayout';
-import SEO from '~src/components/Seo';
-import Link from '~src/components/LocalizedLink';
-import { getLocaleCopy } from '~src/utils/helpers';
-
-
-
