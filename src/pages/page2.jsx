006c172ca95794daf463d27514259407512c6236
Mladen1996 <mladjalug96@gmail.com>
2020-01-12 14:30:03 +0100

    Update page2.jsx

--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -3,20 +3,6 @@ import Layout from '~src/components/Layout/PageLayout';
 import SEO from '~src/components/Seo';
 import Link from '~src/components/LocalizedLink';
 import { getLocaleCopy } from '~src/utils/helpers';
-import { page2 } from '~data/pages';
 
-const SecondPage = ({ pageContext: { locale, name } }) => {
-  const {} = getLocaleCopy(
-    page2,
-    locale
-  );
 
-  return (
-    <Layout name={name}>
-     
-     
-    </Layout>
-  );
-};
 
-export default SecondPage;
